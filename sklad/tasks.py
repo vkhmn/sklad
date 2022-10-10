5abfaee36c95e0459174dc3829b6f8b88dbaa2e6
vkhmn <evokhmin@gmail.com>
2022-10-11 01:07:42 +1000

    Fix email task

--- sklad/tasks.py
+++ sklad/tasks.py
@@ -4,8 +4,12 @@ from django.template.loader import render_to_string
 from sklad_django.celery import app
 from sklad_django.settings import EMAIL_HOST_USER
 from sklad.enams import messages
-from sklad.models import Document
+from sklad.models import Document, Status
 from sklad.utils import get_confirm_url, make_qrcode
+
+
+# TODO:
+# Add new periodic task -> Cancel old orders
 
 
 @app.task
@@ -22,8 +26,10 @@ def send_email_to_buyer(document_id, status):
     except KeyError:
         print(f'Нет одного из ключей: {status}, subject, message')
     else:
-        context['qrcode'] = make_qrcode(document_id)
-        context['url'] = get_confirm_url(document_id)
+        if status == Status.COLLECTED:
+            context['qrcode'] = make_qrcode(document_id)
+            context['url'] = get_confirm_url(document_id)
+
         html_message = render_to_string('sklad/email.html', context)
 
         send_mail(
