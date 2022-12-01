06617d19cdb81d6db213c7864fe90d96f22bbf0b
vkhmn <evokhmin@gmail.com>
2022-12-01 23:39:27 +1000

    Refactoting

--- sklad/config/settings/base.py
+++ sklad/config/settings/base.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 from pathlib import Path
 from os import getenv
 
@@ -91,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
