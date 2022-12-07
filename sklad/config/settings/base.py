ef367f4410f0346d4d40e4de13fdfc5d7a464c57
vkhmn <evokhmin@gmail.com>
2022-12-07 17:28:51 +1000

    Fix

--- sklad/config/settings/base.py
+++ sklad/config/settings/base.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from os import getenv
+from os import getenv, path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -103,6 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
