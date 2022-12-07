6fb53be8958c0243c58312b81092fa47e5433ab0
vkhmn <evokhmin@gmail.com>
2022-12-07 17:13:56 +1000

    Fix

--- sklad/config/settings/prod.py
+++ sklad/config/settings/prod.py
@@ -3,6 +3,6 @@ from config.settings.base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['vkhmn.ru', 'sklad.vkhmn.ru', '127.0.0.1']
-BASE_URL = 'http://vkhmn.ru:8000'
+BASE_URL = 'http://sklad.vkhmn.ru'
 CRON_TIME = dict(minute=0)
 DOCUMENT_TIME_OUT = timedelta(days=2)
